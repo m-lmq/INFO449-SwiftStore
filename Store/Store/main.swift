b79b1075a1348321ab6eafd0d39139207955572c
m-lmq <maggie.lmq0166@gmail.com>
2024-04-15 23:00:28 -0700

    done

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,13 +7,80 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name: String{get}
+    func price() -> Int
+}
 
-class Item {}
 
-class Receipt {}
+class Item: SKU {
+    var name: String
+    var priceEach: Int
 
-class Register {}
+    init(name: String, priceEach: Int) {
+        self.name = name
+        self.priceEach = priceEach
+    }
+
+    func price() -> Int {
+        return priceEach
+    }
+}
+
+
+class Receipt {
+    var items: [SKU] = []
+
+    func allItems() -> [SKU] {
+        return items
+    }
+    
+    func addItem(_ item: SKU) {
+            items.append(item)
+    }
+    
+    func total() -> Int {
+        return items.reduce(0) {$0 + $1.price()}
+    }
+
+    func output() -> String {
+        var recept = "Receipt:\n"
+        for item in items {
+            recept += "\(item.name): $\(Double(item.price()) / 100)\n"
+        }
+        recept += "------------------\n"
+        recept += "TOTAL: $\(Double(total()) / 100)"
+        return recept
+    }
+
+    func clear() {
+        items.removeAll()
+    }
+}
+
+
+class Register {
+    var receipt = Receipt()
+
+    func scan(_ item: SKU) {
+        receipt.addItem(item)
+    }
+
+    func subtotal() -> Int {
+        return receipt.total()
+    }
+
+    func total() -> Receipt {
+        let finalReceipt = receipt
+        receipt = Receipt()
+        return finalReceipt
+    }
+
+    func displaySubtotal() {
+        print("Current subtotal: $\(Double(subtotal()) / 100.0)")
+    }
+}
+
 
 class Store {
     let version = "0.1"
